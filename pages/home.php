fcc01be75c2668047a237e9e9a44b8a96fb3ae05
Jamerson Souza <jamersonsouza@outlook.com>
2020-04-16 15:09:54 -0300

    modificação das paginas

---
+++ pages/home.php
@@ -0,0 +1,148 @@
+<section class="banner-main">
+        <div class="overlay"></div>
+        <div class="center">
+            <form method="post" class="form-main">
+                <h2>Qual seu melhor email?</h2>
+                <input type="email" required name="email">
+                <input type="submit" name="acao" value="Cadastre-se!">
+            </form>
+            </div>
+        </section>
+
+        <div class="container">
+        <section class="descricao-autor">
+            <div class="w50 left">
+                <h2>Jamerson Souza</h2>
+                <p>Lorem ipsum dolor sit amet consectetur
+                    adipisicing elit. Pariatur amet esse et voluptas
+                    , velit odio quisquam mollitia dolor vero soluta e
+                    os, voluptatibus id rerum deserunt consectetur
+                    veritatis error assumenda aliquid.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur
+                    adipisicing elit. Pariatur amet esse et voluptas
+                    , velit odio quisquam mollitia dolor vero soluta e
+                    os, voluptatibus id rerum deserunt consectetur
+                    veritatis error assumenda aliquid.</p>
+
+            </div>
+
+            <div class="w50 left">
+                <!--Imagem -->
+                <img class="right" src="http://localhost/PROJETO_1/assets/myfoto.jpg" alt="" />
+            </div>
+
+            <div class="clear"></div>
+</div>
+        </section>
+
+        <section class="especialidades">
+            <div class="container">
+            <h2 class="title">Especialidades</h2>
+            <div class="w33 left box-espec">
+                <h3><i class="fab fa-css3-alt"></i></h3>
+                <h4>CSS3</h3>
+                <p>Lorem ipsum, dolor sit amet
+                    consectetur adipisicing elit. Earum
+                    repudiandae nesciunt facilis cum por
+                    ro quo laborum vel cupiditate ratione
+                    beatae? Unde veniam recusandae place
+                    at saepe similique sunt numquam aut
+                    sit.</p>
+            </div>
+
+            <div class="box-espec w33 left">
+                <h3><i class="fab fa-html5"></i></h3>
+                <h4>HTML 5 </h3>
+                <p>Lorem ipsum, dolor sit amet
+                    consectetur adipisicing elit. Earum
+                    repudiandae nesciunt facilis cum por
+                    ro quo laborum vel cupiditate ratione
+                    beatae? Unde veniam recusandae place
+                    at saepe similique sunt numquam aut
+                    sit.</p>
+            </div>
+
+
+            <div class="box-espec w33 left">
+                <h3><i class="fab fa-js-square"></i></h3>
+                <h4>Javascript</h3>
+                <p>Lorem ipsum, dolor sit amet
+                    consectetur adipisicing elit. Earum
+                    repudiandae nesciunt facilis cum por
+                    ro quo laborum vel cupiditate ratione
+                    beatae? Unde veniam recusandae place
+                    at saepe similique sunt numquam aut
+                    sit.</p>
+            </div>
+            <div class="clear"></div>
+            </div>
+        </section>
+        <!-- SECTION PARA DEPOIMENTOS E SERVIÇOS -->
+        <section class="extras">
+            <div class="center">
+            <div class="w50 left">
+                <h2 class="title">Depoimentos</h2>
+                <div class="depoimento-single">
+                    <p class="dep-descricao">Lorem ipsum dolor sit amet consectetur
+                        adipisicing elit. Aliquid autem, fugit officia
+                        impedit voluptatibus doloribus provident esse
+                        dolorum qui reprehenderit facere necessitatibus
+                        a ipsam eveniet, ducimus doloremque cumque
+                        quos? Animi.</p>
+                    <p class="nome-autor">Jamerson Souza</p>
+                </div>
+
+                <div class="depoimento-single">
+                    <p class="dep-descricao">Lorem ipsum dolor sit amet consectetur
+                        adipisicing elit. Aliquid autem, fugit officia
+                        impedit voluptatibus doloribus provident esse
+                        dolorum qui reprehenderit facere necessitatibus
+                        a ipsam eveniet, ducimus doloremque cumque
+                        quos? Animi.</p>
+                    <p class="nome-autor">Jamerson Souza</p>
+                </div>
+
+
+                <div class="depoimento-single">
+                    <p class="dep-descricao">Lorem ipsum dolor sit amet consectetur
+                        adipisicing elit. Aliquid autem, fugit officia
+                        impedit voluptatibus doloribus provident esse
+                        dolorum qui reprehenderit facere necessitatibus
+                        a ipsam eveniet, ducimus doloremque cumque
+                        quos? Animi.</p>
+                    <p class="nome-autor">Jamerson Souza</p>
+                </div>
+            </div>
+
+            <!-- INICIO DOS SERVIÇOS FIM DO DEPOIMENTOS -->
+
+
+            <div class="w50 left">
+                <h2 class="title">Serviços</h2>
+                <div class="servicos">
+                    <ul>
+                        <li>Lorem ipsum dolor sit a
+                            met consectetur adipisicing
+                            elit. Porro unde esse volupt
+                            ate enim non delectus volup
+                            tatibus nulla consequatur i
+                        </li>
+                        <li>Lorem ipsum dolor sit a
+                            met consectetur adipisicing
+                            elit. Porro unde esse volupt
+                            ate enim non delectus volup
+                            tatibus nulla consequatur i
+                        </li>
+                        <li>Lorem ipsum dolor sit a
+                            met consectetur adipisicing
+                            elit. Porro unde esse volupt
+                            ate enim non delectus volup
+                            tatibus nulla consequatur i
+                        </li>
+                    </ul>
+                </div>
+                </div>
+                <div class="clear"></div>
+            </div>
+        </section>
+    
