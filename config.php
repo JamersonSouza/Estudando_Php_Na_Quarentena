f4567419a9242b7a055cbd44850fe705ef02a974
Jamerson Souza <jamersonsouza@outlook.com>
2020-04-18 12:40:46 -0300

    configuração da lib phpmailer

--- config.php
+++ config.php
@@ -1,8 +1,19 @@
 <?php
 
+    $autoload = function($class){
+    if($class == 'Email'){
+
+        require_once('classes/phpmailer/PHPMailerAutoload.php');
+
+    }
+
+        include('classes/'.$class.'.php');
+        //echo 'chamou com sucesso';
+    };
+
+    spl_autoload_register($autoload);
+
+
     define('INCLUDE_PATH', 'http://localhost/PROJETO_1/');
-
-
-    
 ?>
-
+ 
